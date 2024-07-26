e4de9d241543ba31d2fe4979a0f6d83f76964416
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-07-26 16:38:30 +0800

    minor change

--- client/src/app/admin/reserved-products/page.tsx
+++ client/src/app/admin/reserved-products/page.tsx
@@ -3,14 +3,40 @@ import React from "react";
 
 const ReservedProducts = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <OrderPreviewCard
-        address="test"
-        customer="test"
-        date="test"
+        customer="Joe Mama"
+        address="Twin Tower"
+        date="09-11-2000"
         image="/images/test_images/roadblock.png"
         price={1000000}
-        productName="test"
+        productName="Boeing 747"
+      />
+      <OrderPreviewCard
+        customer="Joe Mama"
+        address="Twin Tower"
+        date="09-11-2000"
+        image="/images/test_images/roadblock.png"
+        price={1000000}
+        productName="Boeing 747"
+      />
+
+      <OrderPreviewCard
+        customer="Joe Mama"
+        address="Twin Tower"
+        date="09-11-2000"
+        image="/images/test_images/roadblock.png"
+        price={1000000}
+        productName="Boeing 747"
+      />
+
+      <OrderPreviewCard
+        customer="Joe Mama"
+        address="Twin Tower"
+        date="09-11-2000"
+        image="/images/test_images/roadblock.png"
+        price={1000000}
+        productName="Boeing 747"
       />
     </div>
   );
--- client/src/components/cards/OrderPreviewCard.tsx
+++ client/src/components/cards/OrderPreviewCard.tsx
@@ -14,26 +14,26 @@ const OrderPreviewCard = (props: Props) => {
   const { customer, productName, price, date, address, image } = props;
 
   return (
-    <div className="flex p-3 h-[10rem] text-white gap-4 w-full border border-orange">
-      <div className="w-[10%]">
+    <div className="flex p-3 h-[8rem] text-white gap-4 w-full border border-orange rounded-lg">
+      <div className="relative w-[10%]">
         <Image
           src={image}
           alt="image.1"
           width={0}
           height={0}
           sizes="100vw"
-          style={{ width: "100%", height: "auto" }}
-          className="border-none rounded-lg"
+          style={{ width: "100%", height: "100%" }}
+          className="relative border-none rounded-lg"
         />
       </div>
       <div className="w-[40%] flex flex-col gap-3">
         <div>{customer}</div>
         <div>{productName}</div>
-        <div>{price}</div>
+        <div>Price: {price}</div>
       </div>
       <div className="w-[30%]">
-        <div>{date}</div>
-        <div>{address}</div>
+        <div>Date: {date}</div>
+        <div>Address: {address}</div>
       </div>
     </div>
   );
