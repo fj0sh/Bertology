f230902f9cf489215f55524878d8137057e860e3
francis Cutamora <francisjoshuacutamora@gmail.com>
2024-12-05 08:04:17 +0800

    add debugger

--- server/index.js
+++ server/index.js
@@ -18,6 +18,9 @@ const installerRoute = require("./routes/installer.route");
 const mailerRoute = require("./routes/mailer/mailer.route");
 const inquiryRoute = require("./routes/inquiries.route");
 
+console.log(process.env.MAILER_PASS);
+console.log(process.env.MAILER_EMAIL);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
